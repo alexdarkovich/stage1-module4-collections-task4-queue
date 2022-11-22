45d9e3ef84c8095caa98c7396da2ef6695e00238
alexdarkovich <alexdarkovich@mail.ru>
2022-11-22 16:50:18 +0300

    methods written

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,81 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
+
+    public static void main(String[] args) {
+        int numberOfDishes = 11;
+        int everyDishNumberToEat = 4;
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        int count = 1;
+        while (count <= numberOfDishes) {
+            queue.add(count);
+            count++;
+        }
+
+        List<Integer> listPrint = new ArrayList<>();
+        Queue<Integer> toStore = new ArrayDeque<>();
+
+        while (listPrint.size() < numberOfDishes) {
+            if (queue.size() < everyDishNumberToEat) {
+                while (toStore.size() != 0) {
+                    queue.add(toStore.poll());
+                }
+            }
+            if (queue.size() < everyDishNumberToEat) {
+                while (!queue.isEmpty()) {
+                    listPrint.add(queue.poll());
+                }
+                break;
+            }
+            int count2 = 1;
+            while (count2 < everyDishNumberToEat) {
+                toStore.add(queue.poll());
+                count2++;
+            }
+            listPrint.add(queue.poll());
+        }
+        System.out.println(listPrint);
+    }
+
+
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        int count = 1;
+        while (count <= numberOfDishes) {
+            queue.add(count);
+            count++;
+        }
+
+        List<Integer> listPrint = new ArrayList<>();
+        Queue<Integer> toStore = new ArrayDeque<>();
+
+        while (listPrint.size() < numberOfDishes) {
+            if (queue.size() < everyDishNumberToEat) {
+                while (toStore.size() != 0) {
+                    queue.add(toStore.poll());
+                }
+            }
+            if (queue.size() < everyDishNumberToEat) {
+                listPrint.add(queue.poll());
+                toStore.add(queue.poll());
+                listPrint.add(queue.poll());
+                listPrint.add(toStore.poll());
+                break;
+            }
+            int count2 = 1;
+            while (count2 < everyDishNumberToEat) {
+                toStore.add(queue.poll());
+                count2++;
+            }
+            listPrint.add(queue.poll());
+        }
+        return listPrint;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,32 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import javax.swing.*;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class PriorityQueueCreator {
+    public static void main(String[] args) {
+        List<String> firstList = Arrays.asList("Java", "PriorityQueue", "class");
+        List<String> secondList = Arrays.asList("It", "is", "different", "from", "standard", "queues", "where", "FIFO");
+        List<String> third = new ArrayList<>(firstList);
+                third.addAll(secondList);
+        Collections.sort(third);
+        Collections.reverse(third);
+        System.out.println(third);
+
+        Queue<String> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        priorityQueue.addAll(third);
+        System.out.println(priorityQueue);
+    }
+
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        List<String> third = new ArrayList<>(firstList);
+        third.addAll(secondList);
+        Collections.sort(third);
+        Collections.reverse(third);
+
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        priorityQueue.addAll(third);
+        return priorityQueue;
     }
 }
